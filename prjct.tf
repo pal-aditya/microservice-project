0c7fd2e26331ddfc88de19eb0d7035938383d1d0
Aditya <k609627@gmail.com>
2025-08-14 12:10:57 +0000

    Install Kgateway & CNPG

--- prjct.tf
+++ prjct.tf
@@ -31,3 +31,31 @@ resource "kubernetes_manifest" "frontend" {
 resource "kubernetes_manifest" "game-service" {
   manifest = yamldecode(file("~/testing/game-service/service.yml"))
 }
+
+
+data "http" "cnpostgress"{
+  url = "https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.25/releases/cnpg-1.25.3.yaml"
+}
+
+resource "kubernetes_manifest" "postgressinstall"{
+  manifest = yamldecode(data.http.cnpostgress.body)
+}
+
+# Install kgateway CRD's
+resource "helm_release" "kgateway" {
+  name = "kgateway-crds"
+  repository = "oci://cr.kgateway.dev/kgateway-dev/charts"
+  chart      = "kgateway-crds"
+  version    = "2.0.4"
+  namespace  = kubernetes_namespace.kgateway.metadata[0].name
+}
+
+# Install kgateway
+resource "helm_release" "kgateway" {
+  name       = "kgateway"
+  repository = "oci://cr.kgateway.dev/kgateway-dev/charts"
+  chart      = "kgateway"
+  version    = "2.0.4"
+  namespace  = kubernetes_namespace.kgateway.metadata[0].name
+  depends_on = [helm_release.kgateway_crds]
+}
