c8b61431efa87b76067e65823b7c95b85171cddf
Aditya <k609627@gmail.com>
2025-08-27 07:35:43 +0000

    Used the Pods in a Secured way as a nonRootUser

--- prjct.tf
+++ prjct.tf
@@ -15,7 +15,6 @@ resource "kubernetes_manifest" "blunt_games" {
   for_each = fileset("/home/Redis/testing", "*.yml")
   manifest = yamldecode(file("~/testing/${each.value}"))
 }
-
 resource "kubernetes_manifest" "auth-service" {
   manifest = yamldecode(file("~/testing/auth-service/service.yml"))
 }
@@ -33,29 +32,63 @@ resource "kubernetes_manifest" "game-service" {
 }
 
 
-data "http" "cnpostgress"{
-  url = "https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.25/releases/cnpg-1.25.3.yaml"
-}
+#data "external" cnpostgress"{
+#  program = ["bash", "-c" <<EOT
+#  content = $(curl -O https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.25/releases/cnpg-1.25.3.yaml)
+#  EOT
+#  ]
+#}
 
-resource "kubernetes_manifest" "postgressinstall"{
-  manifest = yamldecode(data.http.cnpostgress.body)
-}
+#resource "kubernetes_manifest" "postgressinstall"{
+#  manifest = yamldecode(data.http.cnpostgress.body)
+#}
 
 # Install kgateway CRD's
-resource "helm_release" "kgateway" {
-  name = "kgateway-crds"
-  repository = "oci://cr.kgateway.dev/kgateway-dev/charts"
-  chart      = "kgateway-crds"
-  version    = "2.0.4"
-  namespace  = kubernetes_namespace.kgateway.metadata[0].name
-}
+#resource "helm_release" "kgateway" {
+#  name = "kgateway-crds"
+#  repository = "oci://cr.kgateway.dev/kgateway-dev/charts"
+#  chart      = "kgateway-crds"
+#  version    = "2.0.4"
+#  namespace  = kubernetes_namespace.kgateway.metadata[0].name
+#}
 
 # Install kgateway
-resource "helm_release" "kgateway" {
-  name       = "kgateway"
-  repository = "oci://cr.kgateway.dev/kgateway-dev/charts"
-  chart      = "kgateway"
-  version    = "2.0.4"
-  namespace  = kubernetes_namespace.kgateway.metadata[0].name
-  depends_on = [helm_release.kgateway_crds]
-}
+#resource "helm_release" "kgateway" {
+# name       = "kgateway"
+#  repository = "oci://cr.kgateway.dev/kgateway-dev/charts"
+#  chart      = "kgateway"
+#  version    = "2.0.4"
+#  namespace  = kubernetes_namespace.kgateway.metadata[0].name
+#  depends_on = [helm_release.kgateway_crds]
+#}
+
+#data "external" "cnpostgress" {
+#  program = ["bash", "-c", <<EOT
+#    content=$(curl -s -L https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.25/releases/cnpg-1.25.3.yaml)
+#    jq -n --arg content "$content" '{content:$content}'
+#  EOT
+#  ]
+#}
+
+#resource "kubernetes_manifest" "cnpg" {
+#  manifest = yamldecode(data.external.cnpostgress.result["content"])
+#}
+
+
+
+#data "external" "cert" {
+#  program = ["bash", "-c", <<EOT
+#  content = $(curl -s -L https://github.com/cert-manager/cert-manager/releases/download/v1.18.2/cert-manager.yaml)
+#  jq -n --arg content "$content" '{content:$content}'
+#  EOT
+#  ]
+#}
+
+#resource "kubernetes_manifest" "certmanager" {
+#  manifest = yamldecode(data.external.cert.result["content"])
+#}
+
+
+#resource "kubernetes_manifest" "cnpg"{
+#  manifest = yamldecode(data.external.cnpostgress.content)
+#}
